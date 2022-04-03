fe153093b46c3c6fa9b734c5f912eceaeeaaa8a8
xtern0o <dos30028gmail.com>
2022-04-03 16:49:49 +0300

    stylesheets

--- stylesheet.py
+++ stylesheet.py
@@ -18,3 +18,25 @@ QPushButton:hover {
 	font-weight:bold;
 	text-decoration:none;
 }""")
+
+
+def set_button_delete_stylesheet(btn):
+    btn.setStyleSheet("""QPushButton:!hover { 
+	background-color:#404040;
+	display:inline-block;
+	color:#e6e6e6;
+	font-family:Arial;
+	font-size:19px;
+	font-weight:bold;
+	text-decoration:none;
+}
+
+QPushButton:hover { 
+	background-color:#821a00; 
+	display:inline-block;
+	color:#e6e6e6;
+	font-family:Arial;
+	font-size:19px;
+	font-weight:bold;
+	text-decoration:none;
+}""")
